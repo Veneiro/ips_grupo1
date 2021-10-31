1c60f5d9aca89a54fa0c16e2e186780dad991cb5
Gaspar <UO250825@uniovi.es>
2021-10-31 18:45:22 +0100

    Prototipo de vista de listar jornadas

---
+++ src/main/java/vista/ListaJornadasVista.java
@@ -0,0 +1,68 @@
+package vista;
+
+import java.awt.Rectangle;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+
+import lombok.Getter;
+
+@Getter
+public class ListaJornadasVista extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JTextField textFieldFecha;
+    private JLabel lblSeleccionEmpleado;
+    private JScrollPane scrollPaneCitas;
+    private JPanel panelIntroducirFecha;
+    private JTable table;
+    private JButton btnGestionarCita;
+
+    public ListaJornadasVista() {
+	setTitle("iHospital : Listar Jornadas");
+	inicializar();
+    }
+
+    public void inicializar() {
+	getContentPane().setLayout(null);
+	panelIntroducirFecha = new JPanel();
+	panelIntroducirFecha.setBounds(0, 0, 454, 33);
+	getContentPane().add(panelIntroducirFecha);
+	setBounds(new Rectangle(300, 300, 470, 305));
+	setLocationRelativeTo(null);
+	setResizable(false);
+	setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	panelIntroducirFecha.setLayout(null);
+
+	lblSeleccionEmpleado = new JLabel("B\u00FAsqueda: ");
+	lblSeleccionEmpleado.setBounds(10, 9, 66, 14);
+	lblSeleccionEmpleado.setHorizontalAlignment(SwingConstants.LEFT);
+	panelIntroducirFecha.add(lblSeleccionEmpleado);
+
+	textFieldFecha = new JTextField();
+	textFieldFecha.setBounds(77, 6, 367, 20);
+	panelIntroducirFecha.add(textFieldFecha);
+	textFieldFecha.setColumns(10);
+
+	scrollPaneCitas = new JScrollPane();
+	scrollPaneCitas.setBounds(0, 33, 454, 204);
+	getContentPane().add(scrollPaneCitas);
+
+	table = new JTable();
+	table.setModel(new DefaultTableModel(new Object[][] {},
+		new String[] { "ID", "Empleado", "Comienzo", "Fin", "Entrada", "Salida" }));
+	scrollPaneCitas.setViewportView(table);
+
+	btnGestionarCita = new JButton("Modificar");
+	btnGestionarCita.setBounds(0, 235, 454, 31);
+	getContentPane().add(btnGestionarCita);
+
+    }
+}
