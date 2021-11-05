968603b23357065310d7faac79e7dbc6d7830f93
Gaspar <UO250825@uniovi.es>
2021-11-05 17:05:54 +0100

    Cambios en la vista medicamento es bool, tabla de prescripciones existentes y de pacientes.

--- src/main/java/vista/PrescripcionesVista.java
+++ src/main/java/vista/PrescripcionesVista.java
@@ -3,7 +3,7 @@ package vista;
 import java.awt.Color;
 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,126 +17,120 @@ import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 
 import lombok.Getter;
+import lombok.Setter;
+import util.NoEditableTableModel;
 
 @Getter
 public class PrescripcionesVista extends JDialog {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTable table;
-	private JTextField textField_Paciente;
-	private JTextField textField_Cantidad;
-	private JTextField textField_Intervalo;
-	private JTextField textField_Duracion;
-	private JButton btnNewButton_1;
-	private JTextPane indicacionesTextPane;
-	private JLabel lblNewLabel;
-	private JLabel lblIntervalo;
-	private JLabel lblDuracin;
-	private JButton btnNuevoMedicamento;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTable table;
+    private JTextField textField_Paciente;
+    private JTextField textField_Cantidad;
+    private JTextField textField_Intervalo;
+    private JTextField textField_Duracion;
+    private JButton btnNewButton_1;
+    private JTextPane indicacionesTextPane;
+    private JLabel lblNewLabel;
+    private JLabel lblIntervalo;
+    private JLabel lblDuracin;
+    private JScrollPane scrollPanePacientes;
+    private JTable tablePacientes;
 
-	/**
-	 * Create the frame.
-	 */
-	public PrescripcionesVista() {
-		setTitle("iHospital : Preinscripci\u00F3n");
-		setResizable(false);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setModal(true);
-		setBounds(100, 100, 600, 400);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(211, 211, 211));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+    @Setter
+    private NoEditableTableModel modeloTablaPrescripciones;
 
-		table = new JTable();
-		table.setFillsViewportHeight(true);
-		table.setBorder(new LineBorder(new Color(0, 0, 0)));
-		table.setModel(new DefaultTableModel(new Object[][] { { null },
-				{ null }, { null }, { null }, { null }, { null }, { null },
-				{ null }, { null }, { null }, { null }, { null }, { null },
-				{ null }, { null }, { null }, { null }, { null }, { null },
-				{ null }, { null }, { null }, { null }, { null }, { null }, },
-				new String[] { "Medicamento" }) {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-			boolean[] columnEditables = new boolean[] { false };
+    @Setter
+    private NoEditableTableModel modeloTablaPacientes;
+    private JCheckBox chckbxNewCheckBox;
 
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
-		});
-		table.setBounds(10, 30, 100, 186);
+    /**
+     * Create the frame.
+     */
+    public PrescripcionesVista() {
+	setTitle("iHospital : Preinscripci\u00F3n");
+	setResizable(false);
+	setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	setModal(true);
+	setBounds(100, 100, 850, 400);
+	contentPane = new JPanel();
+	contentPane.setBackground(new Color(211, 211, 211));
+	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(contentPane);
+	contentPane.setLayout(null);
 
-		indicacionesTextPane = new JTextPane();
-		indicacionesTextPane.setBounds(170, 212, 404, 104);
-		contentPane.add(indicacionesTextPane);
+	modeloTablaPrescripciones = new NoEditableTableModel(new String[] { "Nombre" }, 0);
+	table = new JTable(modeloTablaPrescripciones);
+	table.setFillsViewportHeight(true);
+	table.setBorder(new LineBorder(new Color(0, 0, 0)));
+	table.setModel(new DefaultTableModel(new Object[][] {}, new String[] {}));
+	table.setBounds(10, 30, 100, 186);
 
-		JLabel lblNewLabel_1 = new JLabel("Indicaciones");
-		lblNewLabel_1.setBounds(170, 187, 80, 14);
-		contentPane.add(lblNewLabel_1);
+	indicacionesTextPane = new JTextPane();
+	indicacionesTextPane.setBounds(419, 166, 404, 150);
+	contentPane.add(indicacionesTextPane);
 
-		textField_Paciente = new JTextField();
-		textField_Paciente.setBounds(170, 31, 134, 20);
-		contentPane.add(textField_Paciente);
-		textField_Paciente.setColumns(10);
+	JLabel lblNewLabel_1 = new JLabel("Indicaciones");
+	lblNewLabel_1.setBounds(419, 141, 80, 14);
+	contentPane.add(lblNewLabel_1);
 
-		JComboBox<String> comboBox = new JComboBox<String>();
-		comboBox.setBounds(314, 30, 260, 22);
-		contentPane.add(comboBox);
+	textField_Paciente = new JTextField();
+	textField_Paciente.setBounds(170, 15, 220, 20);
+	contentPane.add(textField_Paciente);
+	textField_Paciente.setColumns(10);
 
-		JLabel lblNewLabel_2 = new JLabel("Paciente");
-		lblNewLabel_2.setBounds(170, 11, 100, 14);
-		contentPane.add(lblNewLabel_2);
+	JScrollPane scrollPane = new JScrollPane();
+	scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+	scrollPane.setBounds(10, 11, 150, 339);
+	scrollPane.setViewportView(getTable());
+	contentPane.add(scrollPane);
 
-		btnNuevoMedicamento = new JButton("Nuevo");
-		btnNuevoMedicamento.setBounds(10, 327, 150, 23);
-		contentPane.add(btnNuevoMedicamento);
+	lblNewLabel = new JLabel("Cantidad: ");
+	lblNewLabel.setBounds(429, 52, 61, 14);
+	contentPane.add(lblNewLabel);
 
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setVerticalScrollBarPolicy(
-				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setHorizontalScrollBarPolicy(
-				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(10, 11, 150, 305);
-		scrollPane.setViewportView(getTable());
-		contentPane.add(scrollPane);
+	lblIntervalo = new JLabel("Intervalo: ");
+	lblIntervalo.setBounds(429, 77, 61, 14);
+	contentPane.add(lblIntervalo);
 
-		lblNewLabel = new JLabel("Cantidad: ");
-		lblNewLabel.setBounds(170, 96, 61, 14);
-		contentPane.add(lblNewLabel);
+	lblDuracin = new JLabel("Duraci\u00F3n: ");
+	lblDuracin.setBounds(429, 102, 61, 14);
+	contentPane.add(lblDuracin);
 
-		lblIntervalo = new JLabel("Intervalo: ");
-		lblIntervalo.setBounds(170, 121, 61, 14);
-		contentPane.add(lblIntervalo);
+	textField_Cantidad = new JTextField();
+	textField_Cantidad.setColumns(10);
+	textField_Cantidad.setBounds(554, 46, 260, 20);
+	contentPane.add(textField_Cantidad);
 
-		lblDuracin = new JLabel("Duraci\u00F3n: ");
-		lblDuracin.setBounds(170, 146, 61, 14);
-		contentPane.add(lblDuracin);
+	textField_Intervalo = new JTextField();
+	textField_Intervalo.setColumns(10);
+	textField_Intervalo.setBounds(554, 71, 260, 20);
+	contentPane.add(textField_Intervalo);
 
-		textField_Cantidad = new JTextField();
-		textField_Cantidad.setColumns(10);
-		textField_Cantidad.setBounds(295, 90, 260, 20);
-		contentPane.add(textField_Cantidad);
+	textField_Duracion = new JTextField();
+	textField_Duracion.setColumns(10);
+	textField_Duracion.setBounds(554, 96, 260, 20);
+	contentPane.add(textField_Duracion);
 
-		textField_Intervalo = new JTextField();
-		textField_Intervalo.setColumns(10);
-		textField_Intervalo.setBounds(295, 115, 260, 20);
-		contentPane.add(textField_Intervalo);
+	btnNewButton_1 = new JButton("Confirmar");
+	btnNewButton_1.setBounds(704, 327, 120, 23);
+	contentPane.add(btnNewButton_1);
 
-		textField_Duracion = new JTextField();
-		textField_Duracion.setColumns(10);
-		textField_Duracion.setBounds(295, 140, 260, 20);
-		contentPane.add(textField_Duracion);
+	scrollPanePacientes = new JScrollPane();
+	scrollPanePacientes.setBounds(170, 46, 220, 304);
+	contentPane.add(scrollPanePacientes);
 
-		btnNewButton_1 = new JButton("Confirmar");
-		btnNewButton_1.setBounds(455, 327, 120, 23);
-		contentPane.add(btnNewButton_1);
-	}
+	modeloTablaPacientes = new NoEditableTableModel(new String[] { "Nombre", "Apellido", "Contacto" }, 0);
+	tablePacientes = new JTable(modeloTablaPacientes);
+	tablePacientes.setBounds(0, 0, 1, 1);
+	contentPane.add(tablePacientes);
+
+	chckbxNewCheckBox = new JCheckBox("Medicamento");
+	chckbxNewCheckBox.setBounds(419, 14, 97, 23);
+	contentPane.add(chckbxNewCheckBox);
+    }
 }
