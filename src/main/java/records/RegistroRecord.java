7251c2fd5b83bc11a457aa132822d9d14bd477f2
Gaspar <UO250825@uniovi.es>
2021-11-07 19:55:24 +0100

    REGISTRO AÑADIR ENTRADA A BD

---
+++ src/main/java/records/RegistroRecord.java
@@ -0,0 +1,36 @@
+package records;
+
+import java.util.Objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistroRecord {
+
+    private int id;
+    private String quien, que, fecha, hora;
+
+    @Override
+    public int hashCode() {
+	return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	RegistroRecord other = (RegistroRecord) obj;
+	return id == other.id;
+    }
+
+}
