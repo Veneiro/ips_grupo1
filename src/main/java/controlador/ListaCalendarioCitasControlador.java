8941e278527c1433ffa527c4745423d46043dbb9
Gaspar <UO250825@uniovi.es>
2021-11-07 19:55:24 +0100

    REGISTRO AÑADIR ENTRADA A BD

--- src/main/java/controlador/ListaCalendarioCitasControlador.java
+++ src/main/java/controlador/ListaCalendarioCitasControlador.java
@@ -3,6 +3,7 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 import java.util.Vector;
@@ -11,9 +12,11 @@ import javax.swing.table.DefaultTableModel;
 
 import dtos.CitaDto;
 import dtos.PacienteDto;
+import dtos.RegistroDto;
 import modelo.CitaModelo;
 import modelo.HistorialModelo;
 import modelo.PacienteModelo;
+import modelo.RegistroModelo;
 import util.SwingUtil;
 import vista.AppointmentView;
 import vista.HistorialesVista;
@@ -21,77 +24,80 @@ import vista.ListaCalendarioCitasVista;
 
 public class ListaCalendarioCitasControlador {
 
-	private ListaCalendarioCitasVista lccv;
-	private CitaModelo cm;
-	private PacienteModelo pm;
-	private int idMedico;
-	List<CitaDto> citas;
-	PacienteControlador pc = new PacienteControlador(new PacienteModelo(), new AppointmentView());
-	PacienteDto p = new PacienteDto();
-	
-	public ListaCalendarioCitasControlador(ListaCalendarioCitasVista lccv, CitaModelo cm, PacienteModelo pm
-											, int idMedico) {
-		this.lccv = lccv;
-		this.cm = cm;
-		this.pm = pm;
-		this.idMedico = idMedico;
-		
-		inicializarVistaListaCalendarioCitas();
-	}
-	
-	public void inicializarVistaListaCalendarioCitas() {
-		lccv.setVisible(true);
-	}
-	
-	public void inicializar() {
-		
-		SimpleDateFormat formatter= new SimpleDateFormat("dd-MM-yyyy");
-		Date date = new Date(System.currentTimeMillis());
-		cargarCalendarioCitas(formatter.format(date), idMedico);
-		lccv.getBtnHistorial().addActionListener(new ActionListener() {
+    private ListaCalendarioCitasVista lccv;
+    private CitaModelo cm;
+    private PacienteModelo pm;
+    private RegistroModelo log = new RegistroModelo();
+    private int idMedico;
+    List<CitaDto> citas;
+    PacienteControlador pc = new PacienteControlador(new PacienteModelo(), new AppointmentView());
+    PacienteDto p = new PacienteDto();
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				HistorialControlador  hc = new HistorialControlador(new HistorialModelo(), new HistorialesVista(), idMedico);
-				hc.inicializar();
-			}
-			
-		});
-		lccv.getBtnBuscar().addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String fecha = lccv.getTextFieldFecha().getText();
-				cargarCalendarioCitas(fecha, idMedico);
-				
-			}
-		});
-		lccv.getBtnGestionarCita().addActionListener(
-				e -> SwingUtil.exceptionWrapper(() -> pc.initialize(citas.get(lccv.getTable().getSelectedRow()), p.getNombre())));
-	}
-	
-	private void cargarCalendarioCitas(String fecha, int idMedico) {
-		
-		citas = cm.getCitasFecha(fecha, idMedico);
-		DefaultTableModel dm = new DefaultTableModel(0, 0);
-	    String header[] = new String[] { "Id Cita", "Nombre", "Fecha", "Hora Inicio", "Hora fin",
-	    									"Informacion", "Acudio" };
-	    dm.setColumnIdentifiers(header);
-	    
+    public ListaCalendarioCitasControlador(ListaCalendarioCitasVista lccv, CitaModelo cm, PacienteModelo pm,
+	    int idMedico) {
+	this.lccv = lccv;
+	this.cm = cm;
+	this.pm = pm;
+	this.idMedico = idMedico;
 
-		for (CitaDto c : citas) {
-			PacienteDto p = pm.getPacienteById(c.getId_paciente()).get(0);
-			Vector<Object> data = new Vector<Object>();
-			data.add(c.getId());
-	        data.add(p.getNombre());
-	        this.p.setNombre(p.getNombre());
-	        data.add(c.getFecha());
-	        data.add(c.getHorario_inicio());
-	        data.add(c.getHorario_fin());
-	        data.add(c.getInformacion());
-	        data.add(c.getAcudio());
-			dm.addRow(data);
-		}
-		lccv.getTable().setModel(dm);
+	inicializarVistaListaCalendarioCitas();
+    }
+
+    public void inicializarVistaListaCalendarioCitas() {
+	log.addRegistro(
+		new RegistroDto("Médico " + idMedico, "Consulta calendario de citas", Date.from(Instant.now())));
+	lccv.setVisible(true);
+    }
+
+    public void inicializar() {
+
+	SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+	Date date = new Date(System.currentTimeMillis());
+	cargarCalendarioCitas(formatter.format(date), idMedico);
+	lccv.getBtnHistorial().addActionListener(new ActionListener() {
+
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		HistorialControlador hc = new HistorialControlador(new HistorialModelo(), new HistorialesVista(),
+			idMedico);
+		hc.inicializar();
+	    }
+
+	});
+	lccv.getBtnBuscar().addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		String fecha = lccv.getTextFieldFecha().getText();
+		cargarCalendarioCitas(fecha, idMedico);
+
+	    }
+	});
+	lccv.getBtnGestionarCita().addActionListener(e -> SwingUtil
+		.exceptionWrapper(() -> pc.initialize(citas.get(lccv.getTable().getSelectedRow()), p.getNombre())));
+    }
+
+    private void cargarCalendarioCitas(String fecha, int idMedico) {
+
+	citas = cm.getCitasFecha(fecha, idMedico);
+	DefaultTableModel dm = new DefaultTableModel(0, 0);
+	String header[] = new String[] { "Id Cita", "Nombre", "Fecha", "Hora Inicio", "Hora fin", "Informacion",
+		"Acudio" };
+	dm.setColumnIdentifiers(header);
+
+	for (CitaDto c : citas) {
+	    PacienteDto p = pm.getPacienteById(c.getId_paciente()).get(0);
+	    Vector<Object> data = new Vector<Object>();
+	    data.add(c.getId());
+	    data.add(p.getNombre());
+	    this.p.setNombre(p.getNombre());
+	    data.add(c.getFecha());
+	    data.add(c.getHorario_inicio());
+	    data.add(c.getHorario_fin());
+	    data.add(c.getInformacion());
+	    data.add(c.getAcudio());
+	    dm.addRow(data);
 	}
-	
+	lccv.getTable().setModel(dm);
+    }
+
 }
---
+++ src/main/java/dtos/RegistroDto.java
@@ -0,0 +1,19 @@
+package dtos;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistroDto {
+
+    private String quien, que;
+    private Date fecha;
+
+}
