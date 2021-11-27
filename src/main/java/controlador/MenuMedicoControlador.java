acd177648487946f62ce3210a5405951be3df921
Veneiro <50624734+Veneiro@users.noreply.github.com>
2021-11-27 16:46:17 +0100

    changes

--- src/main/java/controlador/MenuMedicoControlador.java
+++ src/main/java/controlador/MenuMedicoControlador.java
@@ -1,26 +1,33 @@
 package controlador;
 
 import util.SwingUtil;
+import vista.BandejaDeEntradaVista;
+import vista.MensajeCompletoVista;
+import vista.MensajeEnviar;
 import vista.MenuMedicoVista;
 
 public class MenuMedicoControlador {
 
 	private MenuMedicoVista mmv;
 	private ListaCalendarioCitasControlador lcc;
+	private MensajesControlador mc;
 
 	public MenuMedicoControlador(MenuMedicoVista mmv,
 			ListaCalendarioCitasControlador lcc) {
-
+		this.mc = new MensajesControlador(new BandejaDeEntradaVista(),
+				new MensajeCompletoVista(), new MensajeEnviar());
 		this.mmv = mmv;
 		this.lcc = lcc;
 	}
-	
+
 	public void initializate() {
 		mmv.getBtnGestion().addActionListener(
 				e -> SwingUtil.exceptionWrapper(() -> lcc.inicializar()));
+		mmv.getBtnMensajeria().addActionListener(e -> SwingUtil
+				.exceptionWrapper(() -> mc.showBandejaDeEntrada()));
 		lcc.actionProponer(mmv);
 		lcc.citasMod(mmv);
-		
+
 		mmv.setLocationRelativeTo(null);
 		mmv.setVisible(true);
 	}
